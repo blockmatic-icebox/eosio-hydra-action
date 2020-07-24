e82d3eb77e1e93cc3ad960014f5228760d6fe4bb
gaboesquivel <contact@gaboesquivel.com>
2020-07-23 21:28:19 -0600

    chore: add logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Build the smart contracts
+echo 'Building contracts ...'
 mkdir contracts/build
 cd contracts/build
 cmake ..
 make
 
 # Run Hydra tests
+echo 'Testing contracts ...'
 cd ../..
 npm test
